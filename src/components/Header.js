0bf8cafa24366a90549e2c7de15463f7e88eb7dc
Zoltan Simon <zoltan@zoltansimon.me>
2021-08-24 12:33:09 +0200

    disable navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,6 @@ import { Link } from 'gatsby'
 const Header = (props) => (
     <header id="header" className="alt">
         <Link to="/" className="logo"><strong>Timu</strong> <span>.IO</span></Link>
-        <nav>
-            <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
-        </nav>
     </header>
 )
 
