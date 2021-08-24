c101802f730602759386ec24ed34d69664fc1d97
Zoltan Simon <zoltan@zoltansimon.me>
2021-08-24 12:32:35 +0200

    add timu.io in template

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="TIMU.IO"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
